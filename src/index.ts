8f120fd7c7922c48c78f7e27af56f1689a8a2a85
Susisu <susisu2413@yahoo.co.jp>
2026-01-25 16:57:45 +0900

    feat: set up pixi.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-// eslint-disable-next-line no-console
-console.log("Hello, world");
-export {};
+import { Application } from "pixi.js";
+
+async function main(): Promise<void> {
+  const app = new Application();
+  await app.init({ background: "#000000", resizeTo: window });
+  document.body.appendChild(app.canvas);
+}
+
+main().catch((err: unknown) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+});
